e966086780f808775971633c6500394f463a671d
Sushant Mishra <mishrasushant1901@gmail.com>
2022-04-01 23:14:30 +0530

    Preorder Traversal

---
+++ src/Tree/tree_02.java
@@ -0,0 +1,49 @@
+package Tree;
+
+import com.sun.source.tree.BinaryTree;
+
+    public class tree_02{
+        static class Node{
+            int data;
+            Node left;
+            Node right;
+            Node(int data){
+                this.data =data;
+                this.left=null;
+                this.right=null;
+            }
+        }
+
+        public static void preorderTraversal(tree_02.Node root){
+            if(root == null){
+                return;
+            }
+            System.out.println(root.data);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        public static void main(String[] args) {
+            int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+            preorderTraversal(root);
+
+        }
+
+
+
+    }
+
+    public static void preorderTraversal(Node root){
+        if(root == null){
+            return;
+        }
+        System.out.println(root.data);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
+    }
+    public static void main(String[] args) {
+        int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        Node root = null;
+        preorderTraversal(root);
+
+    }
+}
