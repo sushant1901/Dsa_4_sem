190e3ebb8742c239151c707c63d4757d441d99a9
Sushant Mishra <mishrasushant1901@gmail.com>
2022-04-28 19:39:53 +0530

    implementation of Graph using Adjacency list

---
+++ src/Graph/Graph.java
@@ -0,0 +1,2 @@
+package Graph;public class Graph {
+}
--- src/Graph/graph.java
+++ src/Graph/graph.java
@@ -1,48 +1,38 @@
 package Graph;
-public class graph {
-    private boolean adjMatrix[][];
-    private int numVertices;
+import java.util.*;
 
-    // Initialize the matrix
-    public graph(int numVertices) {
-        this.numVertices = numVertices;
-        adjMatrix = new boolean[numVertices][numVertices];
+class Graph {
+
+    static void addEdge(ArrayList<ArrayList<Integer> > adj, int u, int v)
+    {
+        adj.get(u).add(v);
+        adj.get(v).add(u);
     }
 
-    // Add edges
-    public void addEdge(int i, int j) {
-        adjMatrix[i][j] = true;
-        adjMatrix[j][i] = true;
-    }
-
-    // Remove edges
-    public void removeEdge(int i, int j) {
-        adjMatrix[i][j] = false;
-        adjMatrix[j][i] = false;
-    }
-
-    // Print the matrix
-    public String toString() {
-        StringBuilder s = new StringBuilder();
-        for (int i = 0; i < numVertices; i++) {
-            s.append(i + ": ");
-            for (boolean j : adjMatrix[i]) {
-                s.append((j ? 1 : 0) + " ");
+    static void printGraph(ArrayList<ArrayList<Integer>> adj)
+    {
+        for (int i = 0; i < adj.size(); i++) {
+            for (int j = 0; j < adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j)+" ");
             }
-            s.append("\n");
+            System.out.println();
         }
-        return s.toString();
     }
 
-    public static void main(String args[]) {
-        graph g = new graph(4);
+    public static void main(String[] args)
+    {
+        int V = 4;
+        ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer> >(V);
 
-        g.addEdge(0, 1);
-        g.addEdge(0, 2);
-        g.addEdge(1, 2);
-        g.addEdge(2, 0);
-        g.addEdge(2, 3);
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<Integer>());
 
-        System.out.print(g.toString());
+        // Adding edges one by one
+        addEdge(adj, 0, 1);
+        addEdge(adj, 0, 2);
+        addEdge(adj, 1, 2);
+        addEdge(adj, 1, 3);
+
+        printGraph(adj);
     }
 }
