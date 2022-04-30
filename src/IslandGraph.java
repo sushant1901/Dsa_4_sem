2861e66eefde273450ca2ebf075a571e75ad6352
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-04-30 16:37:46 +0530

    For finding connected components

---
+++ src/IslandGraph.java
@@ -0,0 +1,48 @@
+import java.util.*; 
+
+class Graph { 
+
+	static void addEdge(ArrayList<ArrayList<Integer>> adj, int u, int v) 
+	{ 
+		adj.get(u).add(v); 
+		adj.get(v).add(u); 
+	} 
+
+	static void DFSRec(ArrayList<ArrayList<Integer>> adj,int s, boolean[] visited) 
+	{ 
+    	visited[s]=true;
+        
+        for(int u:adj.get(s)){
+            if(visited[u]==false)
+                DFSRec(adj,u,visited);
+        } 
+	} 
+	
+	static int DFS(ArrayList<ArrayList<Integer>> adj, int V){
+	    boolean[] visited=new boolean[V]; int count=0;
+    	for(int i = 0; i<V; i++) 
+    		visited[i] = false;
+    		
+        for(int i=0;i<V;i++){
+            if(visited[i]==false)
+                {DFSRec(adj,i,visited);count++;}
+        }
+        return count;
+	}
+
+	public static void main(String[] args) 
+	{  
+		int V = 5; 
+		ArrayList<ArrayList<Integer> > adj = new ArrayList<ArrayList<Integer>>(V); 
+		
+		for (int i = 0; i < V; i++) 
+			adj.add(new ArrayList<Integer>()); 
+
+			addEdge(adj,0,1); 
+        	addEdge(adj,0,2); 
+        	addEdge(adj,1,2);
+        	addEdge(adj,3,4);
+		
+		System.out.println("Number of connected components: "+DFS(adj,V));
+	} 
+} 
