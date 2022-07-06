a03d84d33a57f2045f7bb6f3cc4e806e0e747fbe
Sushant Mishra <88487239+sushant1901@users.noreply.github.com>
2022-07-06 23:14:27 +0530

    Implementation of open addressing

--- src/HashMap/hashMap1.java
+++ src/HashMap/hashMap1.java
@@ -1,47 +1,85 @@
-// Java program to demonstrate implementation of our 
-// own hash table with chaining for collision detection 
 import java.util.*; 
+import java.io.*; 
 
 class MyHash
 {
-    int BUCKET;
-    ArrayList<LinkedList<Integer>> table;
-    MyHash(int b)
+    int[] arr;
+    int cap,size;
+    
+    MyHash(int c)
     {
-        BUCKET = b;
-        table = new ArrayList<LinkedList<Integer>>();
-        for (int i=0; i<b; i++)
-           table.add(new LinkedList<Integer>());
+        cap=c;
+        size=0;
+        arr=new int[cap];
+        for(int i=0;i<cap;i++)
+            arr[i]=-1;
     }
-    void insert(Integer k)
-    {
-        int i = k % BUCKET;
-        table.get(i).add(k);
+    
+    int hash(int key){
+        return key%cap;
     }
-    boolean search(Integer k)
+    
+    boolean insert(int key)
     {
-        int i = k % BUCKET;
-        return table.get(i).contains(k);
+        if(size==cap)
+            return false;
+        int i=hash(key);
+        while(arr[i]!=-1 && arr[i]!=-2 && arr[i]!=key)
+            i=(i+1)%cap;
+        if(arr[i]==key)
+            return false;
+        else{
+            arr[i]=key;
+            size++;
+            return true;
+        }
     }
-    void delete(Integer k)
+    boolean search(int key)
     {
-        int i = k % BUCKET;
-        table.get(i).remove(k);
+        int h=hash(key);
+        int i=h;
+        while(arr[i]!=-1){
+            if(arr[i]==key)
+                return true;
+            i=(i+1)%cap;
+            if(i==h)
+                return false;
+        }
+        return false;
+    }
+    boolean erase(int key)
+    {
+        int h=hash(key);
+        int i=h;
+        while(arr[i]!=-1){
+            if(arr[i]==key){
+                arr[i]=-2;
+                return true;
+            }
+            i=(i+1)%cap;
+            if(i==h)
+                return false;
+        }
+        return false;
     }
 }
 
 class GFG
 {
-	// Driver method to test Map class 
 	public static void main(String[] args) 
 	{ 
 	    MyHash mh = new MyHash(7);
-	    mh.insert(10);
-	    mh.insert(20);
-	    mh.insert(15);
-	    mh.insert(7);
-	    System.out.println(mh.search(10));
-	    mh.delete(15);
-	    System.out.println(mh.search(15));
+	    mh.insert(49);
+	    mh.insert(56);
+	    mh.insert(72);
+	    if(mh.search(56)==true)
+	        System.out.println("Yes");
+	    else
+	        System.out.println("No");
+	    mh.erase(56);
+	    if(mh.search(56)==true)
+	        System.out.println("Yes");
+	    else
+	        System.out.println("No");
 	} 
 } 
